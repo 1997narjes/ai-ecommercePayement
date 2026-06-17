f82752c9fd178dcf2d1dc0e907ac96fe86f591e6
1997narjes <narjeskhenessi@gmail.com>
2026-06-17 23:08:05 +0100

    fix: remove env files

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://accurate-smile-production-47a8.up.railway.app/api',
 });
 
-// Ajoute le token automatiquement à chaque requête
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  console.log('Token:', token); // Pour déboguer
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
